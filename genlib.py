10ce5fdbb1b05836979e907b1332d906a5862aba
rosrobot <rosrobot.su@gmail.com>
2022-03-15 15:47:38 +0300

    grammar fixes

--- genlib.py
+++ genlib.py
@@ -1,23 +1,23 @@
 """
 functions for creating creatures and generations.
 
-create_creture() return creature))))
-create_generation return generation)))
+create_creature() return creature (pandas Series)
+create_generation return generation (pandas DataFrame)
 """
 
-__version__ = 0.1
-__author__ = 'Rosrobotos'
+__version__ = 0.11
+__author__ = 'github.com/rosrobotos'
 
 import pandas as pd
 import random as rnd
 
 
-def create_creture(gen_lengh=10, creature_name='one'):
-    creature = pd.Series(data=[rnd.randint(0, 1) for i in range(gen_lengh)], name=creature_name)
+def create_creature(gen_length=10, creature_name='one'):
+    creature = pd.Series(data=[rnd.randint(0, 1) for i in range(gen_length)], name=creature_name)
     return creature
 
 
 def create_generation(generation_size=50):
-    generation = pd.DataFrame(data=[create_creture(creature_name=(str(i))+'n') for i in range(generation_size)])
+    generation = pd.DataFrame(data=[create_creature(creature_name=(str(i+1)) + 'th') for i in range(generation_size)])
     generation['quality'] = generation.sum(axis=1)
     return generation
--- main.py
+++ main.py
@@ -3,14 +3,8 @@ I'm trying build genetic algorithm again....
 
 """
 
-__version__ = 0.1
-__author__ = 'Rosrobotos'
-
-# import nympy.matplotlib as mpl
-# import pygame
-# import deep
-import pandas as pd
-import numpy as np
+__version__ = 0.2
+__author__ = 'github.com/rosrobotos'
 
 import genlib
 
@@ -23,10 +17,9 @@ if __name__ == '__main__':
     print('start \n')
     print('create testing creature \n')
 
-    test_creatre = genlib.create_creture()
-    print(test_creatre)
+    test_creature = genlib.create_creature()
+    print(test_creature)
 
     print('create test generation \n')
     test_generation = genlib.create_generation()
-    print(test_generation.sort_values('quality', ascending=False))
-
+    print(test_generation)
