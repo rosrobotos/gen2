abcbedd5491ac5f134e495effc81dae4262b26c8
rosrobot <rosrobot.su@gmail.com>
2022-03-15 14:34:23 +0300

    with summury

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ __author__ = 'Rosrobotos'
 # import nympy.matplotlib as mpl
 # import pygame
 # import deep
+import pandas as pd
+import numpy as np
 
 import genlib
 
@@ -16,7 +18,6 @@ GENOM_SIZE = 10
 GENERATION_SIZE = 50
 GENERATIONS_COUNT = 50
 MUTATION_CHANCE = 0.1
-
 
 if __name__ == '__main__':
     print('start \n')
@@ -30,4 +31,13 @@ if __name__ == '__main__':
     print(test_generation)
     print(test_generation.describe())
 
-help(genlib)
+# test_generation['Summury'] = test_generation.apply(lambda x: sum(x), axis=0)
+# test_generation = test_generation.assign(Summury = lambda x: (sum(x)))
+# print(test_generation)
+
+# test_generation.columns.append()
+
+# test_generation.iloc[i].sum()
+
+#test_generation['summury'] = np.where(test_generation.sum())
+print(test_generation.sum(axis=1))
