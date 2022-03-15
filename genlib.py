a8924ed9fe3eb0fa18ef582a095877a130afcf3f
rosrobot <rosrobot.su@gmail.com>
2022-03-15 16:00:30 +0300

    TABS

--- genlib.py
+++ genlib.py
@@ -13,11 +13,21 @@ import random as rnd
 
 
 def create_creature(gen_length=10, creature_name='one'):
-    creature = pd.Series(data=[rnd.randint(0, 1) for i in range(gen_length)], name=creature_name)
+    creature = pd.Series(
+                        data=[
+                             rnd.randint(0, 1) for i in range(gen_length)
+                             ],
+                        name=creature_name
+                        )
     return creature
 
 
 def create_generation(generation_size=50):
-    generation = pd.DataFrame(data=[create_creature(creature_name=(str(i+1)) + 'th') for i in range(generation_size)])
+    generation = pd.DataFrame(
+                              data=[
+                                   create_creature(creature_name=(str(i + 1)) + 'th')
+                                   for i in range(generation_size)
+                                   ]
+                             )
     generation['quality'] = generation.sum(axis=1)
     return generation
