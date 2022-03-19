7e0ff935755bc5cb65279a2f309dcb68b8bfdbb9
rosrobot <rosrobot.su@gmail.com>
2022-03-19 15:47:48 +0300

    added create_many_generations func

--- genlib.py
+++ genlib.py
@@ -8,8 +8,10 @@ create_generation return generation (pandas DataFrame)
 __version__ = 0.12
 __author__ = 'github.com/rosrobotos'
 
-import pandas as pd
 import random as rnd
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 pd.plotting.register_matplotlib_converters()
 
@@ -17,19 +19,21 @@ pd.plotting.register_matplotlib_converters()
 def create_creature(gen_length=10, creature_name='one'):
     creature = pd.Series(
         data=[
-            rnd.randint(0, 1) for i in range(gen_length)
-        ],
+              rnd.randint(0, 1) for i in range(gen_length)
+             ],
         name=creature_name
     )
 
     return creature
 
 
-def create_generation(generation_size=50):
+def create_generation(generation_size=50, gen_length=10):
     generation = pd.DataFrame(
         data=[
-            create_creature(creature_name=(str(i + 1)) + 'th')
-            for i in range(generation_size)
+            create_creature(
+                            creature_name=(str(i + 1)) + 'th',
+                            gen_length=gen_length
+                            ) for i in range(generation_size)
         ]
     )
     generation['quality'] = generation.sum(axis=1)
@@ -37,13 +41,34 @@ def create_generation(generation_size=50):
     return generation
 
 
-def describe_generation(generation):
+def quality_count(generation):
     """ print quality values, and indexes for any of this values"""
 
     print('\n count of any quality')
     print(generation.quality.value_counts())
+    print('\n')
 
+
+def indexes_of_quality(generation):
     for i in generation.quality.unique():
         print(' quality = ', i, end=': ')
         print(generation.loc[generation.quality == i, 'quality'].index.values)
         print('__')
+
+
+def create_many_generations(number_of_generations=10, generation_size=50, gen_length=10):
+    for i in range(number_of_generations):
+        k = create_generation(
+                              generation_size=generation_size,
+                              gen_length=gen_length
+                             )
+        quality_count(k)
+
+
+def one_generation_pyplot(generation):
+    sns.barplot(
+                x=generation.index,
+                y=generation.sort_values('quality').quality
+               )
+    plt.show()
+
--- main.py
+++ main.py
@@ -6,29 +6,18 @@ I'm trying build genetic algorithm again....
 __version__ = 0.21
 __author__ = 'github.com/rosrobots'
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-
 import genlib
 
 GENOM_SIZE = 10
-GENERATION_SIZE = 50
-GENERATIONS_COUNT = 50
+GENERATION_SIZE = 10
+GENERATIONS_COUNT = 10
 MUTATION_CHANCE = 0.1
 
 if __name__ == '__main__':
     print('start \n')
-    print('create testing creature \n')
 
-    test_creature = genlib.create_creature()
-    print(test_creature)
-
-    print('create test generation \n')
-    test_generation = genlib.create_generation()
-    print(test_generation)
-
-    #   sns.barplot(x=test_generation.index, y=test_generation.sort_values('quality').quality)
-    #   plt.show()
-
-    genlib.describe_generation(test_generation)
+    genlib.create_many_generations(
+                                    number_of_generations=GENERATIONS_COUNT,
+                                    generation_size=GENERATION_SIZE,
+                                    gen_length=GENOM_SIZE
+                                  )
