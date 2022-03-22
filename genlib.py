0111bb8b3caa5a8ef80b084548c58755ac353df8
rosrobot <rosrobot.su@gmail.com>
2022-03-22 16:01:39 +0300

    tabs

--- genlib.py
+++ genlib.py
@@ -5,7 +5,7 @@ create_creature() return creature (pandas Series)
 create_generation return generation (pandas DataFrame)
 """
 
-__version__ = 0.12
+__version__ = 0.14
 __author__ = 'github.com/rosrobotos'
 
 import random as rnd
@@ -16,55 +16,68 @@ import seaborn as sns
 pd.plotting.register_matplotlib_converters()
 
 
-def create_creature(gen_length=10, creature_name='one'):
+def create_creature(
+                    gen_length=10,
+                    creature_name='one'
+                    ):
     creature = pd.Series(
-        data=[
-              rnd.randint(0, 1) for i in range(gen_length)
-             ],
-        name=creature_name
-    )
-
+                         data=[rnd.randint(0, 1) for i in range(gen_length)],
+                         name=creature_name
+                        )
     return creature
 
 
-def create_generation(generation_size=50, gen_length=10):
+def create_generation(
+                      generation_size=50,
+                      gen_length=10
+                     ):
     generation = pd.DataFrame(
-        data=[
-              create_creature(
-                              creature_name=(str(i + 1)) + 'th',
-                              gen_length=gen_length
-                             ) for i in range(generation_size)
-             ]
-    )
+                              data=[
+                                    create_creature(
+                                                    creature_name=(str(i + 1)) + 'th',
+                                                    gen_length=gen_length
+                                                   ) for i in range(generation_size)
+                                   ]
+                             )
     generation['quality'] = generation.sum(axis=1)
-
     return generation
 
 
 def print_quality_count(generation):
     """ print quality values, and indexes for any of this values"""
 
-    print('\n count of any quality')
-    print(generation.quality.value_counts())
-    print('\n')
+    print(
+          '\n',
+          'count of any quality',
+          generation.quality.value_counts(),
+          '\n'
+         )
 
 
 def indexes_of_quality(generation):
     for i in generation.quality.unique():
-        print(' quality = ', i, end=': ')
-        print(generation.loc[generation.quality == i, 'quality'].index.values)
-        print('__')
+        print(
+              'quality = ', i, ': ',
+              generation.loc[generation.quality == i, 'quality'].index.values,
+              '\n',
+              '__'
+             )
 
 
-def create_many_generations(number_of_generations=10, generation_size=50, gen_length=10):
+def create_many_generations(
+                            number_of_generations=10,
+                            generation_size=50,
+                            gen_length=10
+                           ):
     list_of_dataframes = pd.Series(
-        data=[
-              create_generation(
-                                generation_size=generation_size,
-                                gen_length=gen_length
-                               ) for i in range(number_of_generations)
-             ],
-        name='creature_name')
+                                    data=[
+                                        create_generation(
+                                                          generation_size=generation_size,
+                                                          gen_length=gen_length
+                                                         ) for i in range(number_of_generations)
+                                         ],
+                                    name='creature_name'
+                                 )
 
     return list_of_dataframes
 
@@ -79,7 +92,5 @@ def one_generation_pyplot(generation):
 
 def many_generations_pyplot(list_of_generations):
     qualities = [sum(generation.quality) for generation in list_of_generations]
-    sns.lineplot(
-                data=qualities,
-                )
+    sns.lineplot(data=qualities)
     plt.show()
